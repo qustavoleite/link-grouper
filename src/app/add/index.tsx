69a529718c84105460c794ea411610792644751c
Gustavo Leite <qustavoleite@gmail.com>
2025-09-08 00:23:41 -0300

    feat: adcionando navegação no icone de adicionar link

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,24 +1,22 @@
-import {View, Text, TouchableOpacity} from "react-native"
-import { MaterialIcons } from "@expo/vector-icons"
-import { styles } from "./style"
-import { colors } from "@/styles/colors"
+import { Text, TouchableOpacity, View } from 'react-native'
+import { router } from "expo-router"
+import { styles } from './styles'
+import { MaterialIcons } from '@expo/vector-icons'
+import { colors } from '@/styles/colors'
+import { Categories } from '@/components/categories'
 
 export default function Add() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <TouchableOpacity>
-                    <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]}/>
-                </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <MaterialIcons name='arrow-back' size={32} color={colors.gray[200]} />
+        </TouchableOpacity>
+        <Text style={styles.title}>Novo</Text>
+      </View>
 
-                <Text style={styles.title}>
-                    Novo
-                </Text>
-
-                <Text style={styles.label}>
-                    Selecione uma categoria
-                </Text>
-            </View>
-        </View>
-    )
+        <Text style={styles.label}>Selecione uma categoria</Text>
+        <Categories/>
+    </View>
+  )
 }
