4814401d0ed6600d1adf33dad48dc4b304bfd7fc
Gustavo Leite <qustavoleite@gmail.com>
2025-09-11 21:03:41 -0300

    fix: ajustes nas funções get e save

--- src/storage/link-storage.ts
+++ src/storage/link-storage.ts
@@ -11,8 +11,7 @@ export type LinkStorage = {
 
 async function get(): Promise<LinkStorage[]> {
   const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY)
-  const response = storage ? JSON.parse(storage) : null
-
+  const response: LinkStorage[] = storage ? JSON.parse(storage) : []
   return response
 }
 
@@ -20,7 +19,6 @@ async function save(newLink: LinkStorage) {
   try {
     const storage = await get()
     const updated = JSON.stringify([...storage, newLink])
-
     await AsyncStorage.setItem(LINKS_STORAGE_KEY, updated)
   } catch (error) {
     throw error
